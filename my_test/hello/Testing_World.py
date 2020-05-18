6a7e96fdf58e043df6c19a0266ae4a2750653afc
DiegoAlessandro <diegobacigalupo8@gmail.com>
2020-05-18 16:16:25 +0900

    feature: show error message

--- my_test/hello/Testing_World.py
+++ my_test/hello/Testing_World.py
@@ -1,33 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import json
-import testing 
-import Nitta 
+from my_test.hello import testing
+from my_test.hello import Nitta
 
 try:
- email = "saikio4706"
- password = "nfgwe4706"
- sendmail_add = "saikio4706@gmail.com"
+    email = "saikio4706"
+    password = "nfgwe4706"
+    sendmail_add = "saikio4706@gmail.com"
 
- Test_message = testing.scraping( email, password)
- print(Test_message)
+    Test_message = testing.scraping(email, password)
+    print(Test_message)
 
- if Test_message == 'OK_Google':
+    if Test_message == 'OK_Google':
 
-   my_code = Nitta.mail_send(sendmail_add, email, password)
-   print(my_code)
+        my_code = Nitta.mail_send(sendmail_add, email, password)
+        print(my_code)
 
-   if my_code == 'Send_OK':
-     message_num = '0'
-   else:
-     message_num = '-1'
+        if my_code == 'Send_OK':
+            message_num = '0'
+        else:
+            message_num = '-1'
 
- else:
-   message_num = '-1'
+    else:
+        message_num = '-1'
 
 except Exception:
-  error_flg = True
-  message_num = '-1'
+    import traceback
+    traceback.print_exc()
+    error_flg = True
+    message_num = '-1'
 
 print(message_num)
-
--- my_test/hello/testing.py
+++ my_test/hello/testing.py
@@ -8,76 +8,76 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException
 
-#スクレイピング(headless)
 
-def scraping( email, password):
+# スクレイピング(headless)
 
- options = Options()
- driver_path = '/app/.chromedriver/bin/chromedriver'
- options = webdriver.ChromeOptions()
- options.add_argument('--headless')
- driver = webdriver.Chrome(options=options, executable_path=driver_path)
-  
- driver.get("https://www.xserver.ne.jp/login_info.php")
- driver.implicitly_wait(3)
+def scraping(email, password):
+    options = Options()
+    driver_path = '/app/.chromedriver/bin/chromedriver'
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    driver = webdriver.Chrome(options=options)
+    # driver = webdriver.Chrome(options=options, executable_path=driver_path)
 
- try:
-  #ログイン
-  elem = driver.find_element_by_id("memberid")
-  elem.clear()
-  elem.send_keys("chm72639")  
-  elem = driver.find_element_by_id("user_password")
-  elem.clear()
-  elem.send_keys("btqqzotp")
+    driver.get("https://www.xserver.ne.jp/login_info.php")
+    driver.implicitly_wait(3)
 
-  elem = driver.find_element_by_id("submit-btn")
-  elem.click()
-  driver.implicitly_wait(3)
- 
-  driver.find_element_by_xpath('//*[@id="main"]/div[4]/div/div[1]/table/tbody/tr[2]/td[5]/a[1]').click()
-  driver.implicitly_wait(3)
- 
-  #wait.until(lambda d: len(driver.window_handles) > 1)
-  handle = driver.window_handles
-  driver.switch_to.window(handle[1])
+    try:
+        # ログイン
+        elem = driver.find_element_by_id("memberid")
+        elem.clear()
+        elem.send_keys("chm72639")
+        elem = driver.find_element_by_id("user_password")
+        elem.clear()
+        elem.send_keys("btqqzotp")
 
-  try:
-   #メールアカウント設定クリック
-   driver.find_element_by_xpath('//*[@id="top_menu"]/div[2]/div[1]/ul/li[1]/a').click()
-  except NoSuchElementException:
-   driver.switch_to_window(handle[0])
-   driver.find_element_by_xpath('//*[@id="top_menu"]/div[2]/div[1]/ul/li[1]/a').click()
-   driver.implicitly_wait(3)
- 
-  #ドメイン選択画面
-  driver.find_element_by_xpath('//*[@id="contents"]/table/tbody/tr[3]/td[3]/a').click()
-  driver.implicitly_wait(3)
-  #メールアカウント追加クリック
-  driver.find_element_by_xpath('//*[@id="tab"]/li[2]/a').click()
-  driver.implicitly_wait(3)
-  print('Nitta1!')
-  elem = driver.find_element_by_name('mail_username_front')
-  elem.clear()
-  elem.send_keys(email)
-  
-  elem = driver.find_element_by_id('mail_password')
-  elem.clear()
-  elem.send_keys(password)
- 
-  elem = driver.find_element_by_name('action_user_mail_add_auth')
-  elem.click()
-  driver.implicitly_wait(3)
-  print('Nitta2!')
-  elem = driver.find_element_by_name('action_user_mail_add_do')
-  elem.click()
-  driver.implicitly_wait(3)
-  print('Nitta3')
+        elem = driver.find_element_by_id("submit-btn")
+        elem.click()
+        driver.implicitly_wait(3)
 
-  my_message = 'OK_Google'
- 
- except Exception:
-  error_flg = True
-  my_message = 'Error'
+        driver.find_element_by_xpath('//*[@id="main"]/div[4]/div/div[1]/table/tbody/tr[2]/td[5]/a[1]').click()
+        driver.implicitly_wait(3)
 
- return my_message
+        # wait.until(lambda d: len(driver.window_handles) > 1)
+        handle = driver.window_handles
+        driver.switch_to.window(handle[1])
 
+        try:
+            # メールアカウント設定クリック
+            driver.find_element_by_xpath('//*[@id="top_menu"]/div[2]/div[1]/ul/li[1]/a').click()
+        except NoSuchElementException:
+            driver.switch_to_window(handle[0])
+            driver.find_element_by_xpath('//*[@id="top_menu"]/div[2]/div[1]/ul/li[1]/a').click()
+            driver.implicitly_wait(3)
+
+        # ドメイン選択画面
+        driver.find_element_by_xpath('//*[@id="contents"]/table/tbody/tr[3]/td[3]/a').click()
+        driver.implicitly_wait(3)
+        # メールアカウント追加クリック
+        driver.find_element_by_xpath('//*[@id="tab"]/li[2]/a').click()
+        driver.implicitly_wait(3)
+        print('Nitta1!')
+        elem = driver.find_element_by_name('mail_username_front')
+        elem.clear()
+        elem.send_keys(email)
+
+        elem = driver.find_element_by_id('mail_password')
+        elem.clear()
+        elem.send_keys(password)
+
+        elem = driver.find_element_by_name('action_user_mail_add_auth')
+        elem.click()
+        driver.implicitly_wait(3)
+        print('Nitta2!')
+        elem = driver.find_element_by_name('action_user_mail_add_do')
+        elem.click()
+        driver.implicitly_wait(3)
+        print('Nitta3')
+
+        my_message = 'OK_Google'
+
+    except Exception:
+        error_flg = True
+        my_message = 'Error'
+
+    return my_message
